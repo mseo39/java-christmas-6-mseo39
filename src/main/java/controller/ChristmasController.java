72952e49873fbe821f5af7332d2d169d1b63a6d8
mseo39 <a01063294905@gmail.com>
2023-11-14 20:03:20 +0900

    feat: 빈칸 출력 요청

--- src/main/java/controller/ChristmasController.java
+++ src/main/java/controller/ChristmasController.java
@@ -1,11 +1,16 @@
 package controller;
 
 import model.User.ChristmasUser;
+import view.OutputView;
 
 public class ChristmasController {
     private static ChristmasUser user;
 
     public ChristmasController() {
         user = new ChristmasUser();
+    }
+
+    public static void requestSection() {
+        OutputView.printSection();
     }
 }
