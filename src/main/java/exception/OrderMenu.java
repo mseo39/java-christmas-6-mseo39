023cd0d9615832a926291e99321f0ad89c4518d3
mseo39 <a01063294905@gmail.com>
2023-11-14 19:04:56 +0900

    feat: 메뉴 수량이 1이상인지 검사

--- src/main/java/exception/OrderMenu.java
+++ src/main/java/exception/OrderMenu.java
@@ -18,4 +18,13 @@ public class OrderMenu {
 
         return itemInfo;
     }
+
+    public static int menuQuantityValid(String input) {
+        int num = InputValid.stringToInteger(input, "주문");
+        if (num < 1) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+
+        return num;
+    }
 }
