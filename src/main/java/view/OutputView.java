d52488ffa5416dc3aeb3cd8ed134959614ccd978
mseo39 <a01063294905@gmail.com>
2023-11-14 19:52:18 +0900

    feat: 혜택 안내 출력

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -13,4 +13,8 @@ public class OutputView {
     public static void printGreeting() {
         System.out.println(Phrase.GREETING.getPhrase());
     }
+
+    public static void printBenefitsGuide(int month, int day) {
+        System.out.println(String.format(Phrase.BENEFITS_GUIDE.getPhrase(), month, day));
+    }
 }
