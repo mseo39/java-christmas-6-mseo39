367922a6d42f51e3d9cae701cf666f96be75829e
mseo39 <a01063294905@gmail.com>
2023-11-14 19:03:37 +0900

    feat: 메뉴 입력형식 검사

---
+++ src/main/java/exception/OrderMenu.java
@@ -0,0 +1,21 @@
+package exception;
+
+import model.menu.*;
+
+import java.util.Map;
+
+public class OrderMenu {
+    private OrderMenu() {
+
+    }
+
+    public static String[] menuFormat(String input) {
+        String[] itemInfo = input.split("-");
+
+        if (itemInfo.length != 2) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+
+        return itemInfo;
+    }
+}
