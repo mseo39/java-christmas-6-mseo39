fe038739c9cc230f7b519b684d30ba25d74cf780
mseo39 <a01063294905@gmail.com>
2023-11-15 03:38:11 +0900

    Test: 디저트개수_확인_테스트

--- src/test/java/model/menu/ChristmasMenuTest.java
+++ src/test/java/model/menu/ChristmasMenuTest.java
@@ -4,6 +4,9 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import christmas.Application;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ChristmasMenuTest extends NsTest {
@@ -24,6 +27,17 @@ public class ChristmasMenuTest extends NsTest {
     void 메뉴가_애피타이저가_아닐때_테스트() {
         boolean isAppetizer = ChristmasMenu.isValidAppetizer("제로콜라");
         assertThat(isAppetizer).isFalse();
+    }
+
+    @Test
+    void 디저트개수_확인_테스트() {
+        Map<String, Integer> order = new HashMap<>();
+        order.put("양송이수프", 1);
+        order.put("초코케이크", 3);
+        order.put("아이스크림", 1);
+
+        int count = ChristmasMenu.getCountForDessert(order);
+        assertThat(count).isEqualTo(4);
     }
 
     @Override
