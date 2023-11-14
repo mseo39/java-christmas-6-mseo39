43b85c432f09086fd2d7f351c16150d6b1ecd351
mseo39 <a01063294905@gmail.com>
2023-11-15 03:28:10 +0900

    Test: 산타배지반환_테스트

---
+++ src/test/java/model/event/BadgeTest.java
@@ -0,0 +1,21 @@
+package model.event;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import christmas.Application;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BadgeTest extends NsTest {
+
+    @Test
+    void 산타배지반환_테스트() {
+        String type = Badge.getBadgeType(20000);
+        assertThat(type).isEqualTo("산타");
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
