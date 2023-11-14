252e53c7f18c50648c07db9d73e541301d2d4a66
mseo39 <a01063294905@gmail.com>
2023-11-14 17:53:44 +0900

    feat: 배지 종류 반환

--- src/main/java/model/event/Badge.java
+++ src/main/java/model/event/Badge.java
@@ -14,4 +14,18 @@ public enum Badge {
     public int getPrice() {
         return price;
     }
+
+    public static String getBadgeType(int price) {
+        if (SANTA.getPrice() <= price) {
+            return "산타";
+        }
+        if (TREE.getPrice() <= price) {
+            return "트리";
+        }
+        if (STAR.getPrice() <= price) {
+            return "별";
+        }
+
+        return "없음";
+    }
 }
