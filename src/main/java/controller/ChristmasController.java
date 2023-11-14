393e06d053c2498c5b49fbff902cd472264d5ba5
mseo39 <a01063294905@gmail.com>
2023-11-14 20:02:43 +0900

    feat: 컨트롤러 생성

---
+++ src/main/java/controller/ChristmasController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import model.User.ChristmasUser;
+
+public class ChristmasController {
+    private static ChristmasUser user;
+
+    public ChristmasController() {
+        user = new ChristmasUser();
+    }
+}
