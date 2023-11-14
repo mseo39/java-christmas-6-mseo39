055078df355333e1f834d1695e3a385a85d102af
mseo39 <a01063294905@gmail.com>
2023-11-14 19:52:57 +0900

    feat: 혜택메뉴 출력

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -17,4 +17,11 @@ public class OutputView {
     public static void printBenefitsGuide(int month, int day) {
         System.out.println(String.format(Phrase.BENEFITS_GUIDE.getPhrase(), month, day));
     }
+
+    public static void printMenu(Map<String, Integer> order) {
+        System.out.println(Phrase.ORDER_MENU.getPhrase());
+        for (Map.Entry<String, Integer> entry : order.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue() + "개");
+        }
+    }
 }
