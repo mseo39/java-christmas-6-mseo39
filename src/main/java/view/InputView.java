02b897078e355eed1b36990b2659116c057e71d6
mseo39 <a01063294905@gmail.com>
2023-11-14 19:48:09 +0900

    feat: 메뉴 입력받기

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -28,4 +28,45 @@ public class InputView {
         }
         return result;
     }
+
+    public static Map<String, Integer> readMenu() {
+        System.out.println(Phrase.INPUT_MENU.getPhrase());
+        Map<String, Integer> orderMap = processMenuInput();
+
+        while (orderMap.isEmpty()) {
+            orderMap = processMenuInput();
+        }
+
+        return orderMap;
+    }
+
+    private static Map<String, Integer> processMenuInput() {
+        String input = Console.readLine();
+        Map<String, Integer> orderMap = new HashMap<>();
+
+        for (String item : input.split(",")) {
+            try {
+                processOrderItem(orderMap, item);
+                checkOrderItem(orderMap);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                orderMap.clear();
+                break;
+            }
+        }
+
+        return orderMap;
+    }
+
+    private static void processOrderItem(Map<String, Integer> orderMap, String item) {
+        String[] itemInfo = OrderMenu.menuFormat(item);
+        OrderMenu.menuValid(itemInfo[0]);
+        OrderMenu.menuDuplication(orderMap, itemInfo[0]);
+        orderMap.put(itemInfo[0], OrderMenu.menuQuantityValid(itemInfo[1]));
+    }
+
+    private static void checkOrderItem(Map<String, Integer> orderMap) {
+        OrderMenu.maxNumMenu(orderMap);
+        OrderMenu.checkDrinkCount(orderMap);
+    }
 }
