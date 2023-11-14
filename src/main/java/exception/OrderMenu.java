ab422a515d1513fb4e94ca08993902ca2d764c3e
mseo39 <a01063294905@gmail.com>
2023-11-14 19:07:27 +0900

    feat: 수량이 20개 초과하는지 검사

--- src/main/java/exception/OrderMenu.java
+++ src/main/java/exception/OrderMenu.java
@@ -42,4 +42,10 @@ public class OrderMenu {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
     }
+
+    public static void maxNumMenu(Map<String, Integer> order) {
+        if (order.values().stream().mapToInt(Integer::intValue).sum() > 20) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
 }
